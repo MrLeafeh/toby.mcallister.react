6769dce4c959cc6d17163ea80b7a34f638441b5c
Toby <tlfmcallister@gmail.com>
2020-03-05 11:35:03 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <header className="container-nav-bar nav-blue">
+      <div className="nav-bar">
+        <a href="index.html">
+          <h2>Home</h2>
         </a>
-      </header>
-    </div>
+        <a href="https://docs.google.com/document/d/1vV9bg_HC5QM-j_he7VWpcDRLAW8odWv6UJZXZJtLHqo/edit?usp=sharing">
+          <h2>Resume</h2>
+        </a>
+        <a href="mailto:tlfmcallister@gmail.com?Subject=Hi%20Toby">
+          <h2>E-Mail</h2>
+        </a>
+      </div>
+    </header>
   );
 }
 
